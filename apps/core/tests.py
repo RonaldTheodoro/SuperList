15cdff7ffe80c7858dd771444e049dcd036694b7
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2017-07-06 22:36:11 -0300

    basic view now returns mininal html

--- apps/core/tests.py
+++ apps/core/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.urls import resolve
+from django.http import HttpRequest
 from .views import index
 
 
@@ -8,3 +9,11 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         response = resolve('/')
         self.assertEqual(response.func, index)
+
+    def test_home_page_returns_correts_html(self):
+        request = HttpRequest()
+        response = index(request)
+        html = response.content.decode('utf-8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
