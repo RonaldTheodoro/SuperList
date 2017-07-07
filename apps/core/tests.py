06bbd355b65b801d32fbaadbc4ee5db3f67e68f6
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2017-07-06 23:35:36 -0300

    refactor index page view to use a template

--- apps/core/tests.py
+++ apps/core/tests.py
@@ -1,19 +1,9 @@
 from django.test import TestCase
-from django.urls import resolve
-from django.http import HttpRequest
 from .views import index
 
 
 class HomePageTest(TestCase):
 
-    def test_root_url_resolves_to_home_page_view(self):
-        response = resolve('/')
-        self.assertEqual(response.func, index)
-
     def test_home_page_returns_correts_html(self):
-        request = HttpRequest()
-        response = index(request)
-        html = response.content.decode('utf-8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'index.html')
