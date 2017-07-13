66fa0184ac94bdca25ae5fe6b78132dfab7cc942
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2017-07-12 21:45:47 -0300

    index page redirect after save item

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.http import HttpResponse
+from .models import Item
 
 
 def index(request):
-    return render(
-        request,
-        'index.html',
-        {'new_item_text': request.POST.get('item_text', '')}
-    )
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+    return render(request, 'index.html')
