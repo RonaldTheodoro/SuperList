d82a28a866af0894860821980f8b2ce43c357a8d
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2017-07-12 22:04:35 -0300

    Redirect after POST, and show all items in template

--- apps/core/views.py
+++ apps/core/views.py
@@ -8,4 +8,5 @@ def index(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
         return redirect('/')
-    return render(request, 'index.html')
+    items = Item.objects.all()
+    return render(request, 'index.html', {'items': items})
