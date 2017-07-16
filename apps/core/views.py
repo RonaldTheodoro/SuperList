5c7a84cd7da6bfa1f5a9a011eeff503b00c1c55d
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2017-07-16 13:57:04 -0300

    new URL, view and template to display lists

--- apps/core/views.py
+++ apps/core/views.py
@@ -7,6 +7,10 @@ from .models import Item
 def index(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
+        return redirect('/lists/user-list')
+    return render(request, 'index.html')
+
+
+def view_list(request):
     items = Item.objects.all()
-    return render(request, 'index.html', {'items': items})
+    return render(request, 'list.html', {'items': items})
