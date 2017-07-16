181443a2868cfe33664422efd8ab43bcc1a0c750
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2017-07-16 14:32:39 -0300

    add new_list view

--- config/urls.py
+++ config/urls.py
@@ -6,5 +6,6 @@ from apps.core import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),
+    url(r'^lists/new$', views.new_list, name='new_list'),
     url(r'^lists/user-list/$', views.view_list, name='view_list'),
 ]
